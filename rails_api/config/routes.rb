43a8e54753b6f541c6f483c5dfbd35894841d147
Devon Norris <norris.devon@gmail.com>
2018-06-02 15:48:38 -0400

    nothing here to worry about

--- rails_api/config/routes.rb
+++ rails_api/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :chatrooms, only: [:index, :create]
+  resources :messages, only: [:create]
+
+  mount ActionCable.server => '/cable'
+
 end
