0ad64dacdfad8276b35050978f56edf272f88761
spierre95 <stephen.pierre20@gmail.com>
2018-06-02 14:44:04 +0000

    updated file structure added routes

---
+++ react_app/src/CreateTrip.jsx
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class CreateTrip extends Component{
+  render(){
+    return(<div><h1>CreateTrip</h1></div>)
+  }
+}
+
+export default CreateTrip;
---
+++ react_app/src/UserInvite.jsx
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class UserInvite extends Component{
+  render(){
+    return(<div><h1>UserInvite</h1></div>)
+  }
+}
+
+export default UserInvite;
