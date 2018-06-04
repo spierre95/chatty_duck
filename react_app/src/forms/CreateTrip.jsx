c3e751391b6ad17b442874442d2cbadb67a7efa0
Yasu <yassantune@gmail.com>
2018-06-04 21:19:46 +0000

    added CreateGroup and AddEmail pages, changed contents of CreateTrip page.

--- react_app/src/forms/CreateTrip.jsx
+++ react_app/src/forms/CreateTrip.jsx
@@ -3,36 +3,34 @@ import Header from '../lp/Header.jsx';
 import Footer from '../lp/Footer.jsx';
 
 
-class CreateTrip extends Component{
-
-  inputEmail = () => {
-    // create max 5 input email field
-    let output = [];
-    for (let i = 1; i <= 5; i++) {
-      output.push(<input type="email" className="form-control fontawesome" id="email-{i}" placeholder="&#xf0e0; Enter Email" />);
-    }
-    return output;
-  }
-
+class CreateTrip extends Component {
   render(){
+    let form = (
+        <form>
+          <div className="form-group">
+            <label htmlFor="trip name">TripName</label>
+            <input type="text" className="form-control" id="trip-name" placeholder="Enter trip name" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="start date">Start Date</label>
+            <input type="date" className="form-control" id="start-name" placeholder="yyyy-mm-dd" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="end date">End Date</label>
+            <input type="date" className="form-control" id="end-date" placeholder="yyyy-mm-dd" />
+          </div>
+          <button type="submit" className="btn btn-primary">Create</button>
+        </form>
+    );
     return (
       <div className="form-page">
         <Header />
-        <aside>
-          <form action='/'>
-            <div className="form-group">
-              <p className="title">Create Group</p>
-              <label htmlFor="email address section">Email Address</label>
-              {this.inputEmail()}
-              <a href="">Add More</a>
-            </div>
-            <button type="submit" className="btn btn-primary">Submit</button>
-          </form>
-        </aside>
+          <section>
+            {form}
+          </section>
         <Footer />
       </div>
     );
   }
 }
-
 export default CreateTrip;
