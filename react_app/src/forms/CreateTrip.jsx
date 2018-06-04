718e92bcb5d7ac5a222160b601fa1a4e629447af
Yasumitsu Hashimoto <YasumitsuHashimoto@Yasumitsus-MacBook-Pro.local>
2018-06-04 15:28:38 -0400

    added layout for createTrip and selectTrip pages

--- react_app/src/forms/CreateTrip.jsx
+++ react_app/src/forms/CreateTrip.jsx
@@ -1,38 +1,38 @@
 import React, {Component} from 'react';
+import Header from '../lp/Header.jsx';
+import Footer from '../lp/Footer.jsx';
+
 
 class CreateTrip extends Component{
-    inputEmail = () => {
+
+  inputEmail = () => {
     // create max 5 input email field
     let output = [];
-    for (let i = 1; i < 5; i++) {
-      output.push(<input type="email" className="form-control fontawesome" id="email-' + i + '" placeholder="&#xf0e0; Enter Email" />);
+    for (let i = 1; i <= 5; i++) {
+      output.push(<input type="email" className="form-control fontawesome" id="email-{i}" placeholder="&#xf0e0; Enter Email" />);
     }
     return output;
   }
 
   render(){
-    let form = '';
-
     return (
-      <aside>
-        <form action='/'>
-          <div className="form-group">
-            <p className="title">Create Group</p>
-            <label htmlFor="email address section">Email Address</label>
-            {this.inputEmail()}
-            <a href="">Add More</a>
-          </div>
-          <button type="submit" className="btn btn-primary">Submit</button>
-        </form>
-      </aside>
+      <div className="form-page">
+        <Header />
+        <aside>
+          <form action='/'>
+            <div className="form-group">
+              <p className="title">Create Group</p>
+              <label htmlFor="email address section">Email Address</label>
+              {this.inputEmail()}
+              <a href="">Add More</a>
+            </div>
+            <button type="submit" className="btn btn-primary">Submit</button>
+          </form>
+        </aside>
+        <Footer />
+      </div>
     );
   }
 }
 
 export default CreateTrip;
-
-          // <input type="email" className="form-control fontawesome" id="email-1" placeholder="&#xf0e0; Enter Email">
-          // <input type="email" className="form-control fontawesome" id="email-2" placeholder="&#xf0e0; Enter Email">
-          // <input type="email" className="form-control fontawesome" id="email-3" placeholder="&#xf0e0; Enter Email">
-          // <input type="email" className="form-control fontawesome" id="email-4" placeholder="&#xf0e0; Enter Email">
-          // <input type="email" className="form-control fontawesome" id="email-5" placeholder="&#xf0e0; Enter Email">
