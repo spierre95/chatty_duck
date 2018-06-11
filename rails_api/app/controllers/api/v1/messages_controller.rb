4e21c5e992fa36476872d165831b71bcd648d4ac
Devon Norris <norris.devon@gmail.com>
2018-06-11 11:56:57 -0400

    not much

--- rails_api/app/controllers/api/v1/messages_controller.rb
+++ rails_api/app/controllers/api/v1/messages_controller.rb
@@ -6,13 +6,13 @@ module Api::V1
     end
 
     def create
-      message = Message.new(message_params)
-      chatroom = Chatroom.find(message_params[:chatroom_id])
+      @message = Message.new(message_params)
+      @chatroom = Chatroom.find(message_params[:chatroom_id])
       if message.save
         serialized_data = ActiveModelSerializers::Adapter::Json.new(
           MessageSerializer.new(message)
         ).serializable_hash
-        MessagesChannel.broadcast_to chatroom, serialized_data
+        MessagesChannel.broadcast_to @chatroom, serialized_data
         head :ok
       end
     end
