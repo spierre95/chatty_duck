73f2c239aca2b2e1e22ba26db6811d21ce0c3896
Yasu <yassantune@gmail.com>
2018-06-14 06:44:15 +0000

    fixed btn overlay issue, added some text in LP

--- react_app/src/lp/Main.jsx
+++ react_app/src/lp/Main.jsx
@@ -16,25 +16,29 @@ class Main extends Component {
           <div className="container">
             <div className="row">
               <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                <p className="title">Hello World. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum provident explicabo sapiente odit repellendus asperiores sit sunt nemo!</p>
+                <p className="title">Do you like traveling with your friends? Do you want to stay connected with your travel buddies while traveling?<br />
+                Why don't you try our new product - Chatty Duck. The app let you connect during your trip!</p>
                 <section>
-                  <div className="row">
+                  <div className="row landing">
                     <div className="card col-xs-12 col-sm-4 col-md-4 col-lg-4">
                       <div className="card-wrapper">
                         <p className="icon"><i className="fa fa-laptop"></i></p>
-                        <p className="desc">text here. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem molestiae facere quisquam tempora error dolor, quis consequuntur delectus, vel est maiores inventore in deserunt odio nobis culpa dolorem! Asperiores, nemo.</p>
+                        <p className="desc">If you have internet connection, you can connect with your travel buddies! <br />
+                        You can create a group chat for your trip and your friends can join your chat group.</p>
                       </div>
                     </div>
                     <div className="card col-xs-12 col-sm-4 col-md-4 col-lg-4">
                       <div className="card-wrapper">
                         <p className="icon"><i className="fa fa-map-marker"></i></p>
-                        <p className="desc">text here. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem molestiae facere quisquam tempora error dolor, quis consequuntur delectus, vel est maiores inventore in deserunt odio nobis culpa dolorem! Asperiores, nemo.</p>
+                        <p className="desc">You can stay in touch with your travel buddies before and during your trip. <br />
+                        You can talk with your travel buddie all at once. This app is made for specifically group travel purpose!</p>
                       </div>
                     </div>
                     <div className="card col-xs-12 col-sm-4 col-md-4 col-lg-4">
                       <div className="card-wrapper">
                         <p className="icon"><i className="fa fa-plane"></i></p>
-                        <p className="desc">text here. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem molestiae facere quisquam tempora error dolor, quis consequuntur delectus, vel est maiores inventore in deserunt odio nobis culpa dolorem! Asperiores, nemo.</p>
+                        <p className="desc">You can create and customize an itinerary for your trip. <br />
+                        This app help your special trip with your friends.</p>
                       </div>
                     </div>
                     <div className="clearfix visible-xs-block"></div>
